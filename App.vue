a03b9a8ef1b8ea3aa871413de504607cd9e57902
lian <msn19882009@live.cn>
2025-08-28 20:09:59 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello Vue 3!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <input v-model="message" />
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
